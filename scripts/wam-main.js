5e2af3d2a316a238ad9f2f7e982c70aa7877d4c4
Byuu La <ladanggiabao@gmail.com>
2020-09-27 15:53:47 +0700

    Enter

--- scripts/wam-main.js
+++ scripts/wam-main.js
@@ -1,16 +1,15 @@
+const malletCursor = document.querySelector('.mallet-cursor');
 const welcomeScreen = document.querySelector('.welcome-screen');
 const gameTitle = document.getElementById('game-title');
 const overlay = document.querySelector('.overlay');
 const credit = document.getElementById('credit');
+const gamemode = document.querySelector('.gamemode');
+
 const startButton = document.querySelector('.startButton');
 const musicButton = document.querySelector('.musicButton');
 const soundButton = document.querySelector('.soundButton');
-const gamemode = document.querySelector('.gamemode');
 const replayButton = document.querySelector('.replayButton');
 const partyButton = document.querySelector('.partyButton');
-const soundfx = new Audio("D:/Work/3.whack-a-mole-gam/smack.mp3")
-const bgtheme = new Audio("D:/Work/3.whack-a-mole-gam/crazy-frog.mp3")
-const malletCursor = document.querySelector('.mallet-cursor');
 
 const holes = document.querySelectorAll('.hole');
 const scoreboard = document.querySelector('.scoreboard');
@@ -18,30 +17,38 @@ const gameboard = document.querySelector('.gameboard-wp');
 const moles = document.querySelectorAll('.mole');
 const countdownBoard = document.querySelector('.countdown');
 
-let musicon = true;
-let soundon = true;
+const audio = 
+    [new Audio("../assets/audio/smack.mp3"),
+    new Audio("../assets/audio/crazy-frog.mp3"),
+    new Audio("../assets/audio/crazy-frog-nightcore.mp3")];
+let musicon = true, soundon = true;
+
+const TIMELIMIT = 15000;
 let lastHole;
 let timeUp = false;
-let difficulty = false;
-let timeLimit = 15000;
+let difficulty= false;
 let score = 0;
 let timesplayed = 0;
-// Menu Module
+
+// UI Module
+
+const strikethroughToggle = element => element.classList.toggle('line-through');
+
 const musicToggle = () => {
     malletAnimation();
     musicon = !musicon;
-    if (!musicon) {bgtheme.pause(); bgtheme.currentTime = 0;}
-    musicon 
-    ? musicButton.style.setProperty("text-decoration", "none") 
-    : musicButton.style.setProperty("text-decoration", "line-through")
+    if (!musicon) stopMusic();
+    strikethroughToggle(musicButton);
 }
-
 const soundToggle = () => {
     malletAnimation();
     soundon = !soundon;
-    soundon 
-    ? soundButton.style.setProperty("text-decoration", "none")
-    : soundButton.style.setProperty("text-decoration", "line-through")
+    strikethroughToggle(soundButton);
+}
+const berzerkToggle = () => {
+    malletAnimation();
+    difficulty = !difficulty;
+    partyButton.classList.toggle('berzerk-deco')
 }
 
 const displayToggle = (...elements) => {
@@ -55,27 +62,67 @@ const opacityToggle = (...element) => {
         ele.classList.toggle('fade-out');
     });
 }
-
-// Cursor Module
+const cdText = (string = '', extra = '') => {
+    (string) ? countdownBoard.textContent += (' ' + extra) 
+    : countdownBoard.textContent = string; 
+}
 
 const cursor = e => {
     malletCursor.style.top = e.pageY + "px";
     malletCursor.style.left = e.pageX + "px";
 }
-function malletAnimation() {
-    if (soundon) {soundfx.play()};
+const malletAnimation = () => {
+    if (soundon) {audio[0].currentTime = 0; audio[0].play()};
     malletCursor.classList.add('mallet-move');
     setTimeout( () => {malletCursor.classList.remove('mallet-move')}, 500);
 }
+const moleAnimation = mole => {mole.classList.toggle('mole-animate');}
+    
+//Sound Module
+const stopMusic = () => {
+    for (let i = 1; i < audio.length; i++) {
+        audio[i].pause();
+        audio[i].currentTime = 0;
+        };
+}
+const playTheme = (music,track) => {if (musicon) {
+    stopMusic();
+    music[track].play();
+    }
+}
 
 // Main Game module
-const startGame = () => {
+const gameStart = () => {
     malletAnimation();
-    gameInit();
+    timeUp = false;
+    score = 0;
     timesplayed++;
-    setTimeout(() => countdownBoard.textContent = 'SMACK THEM!!', 2800);
-    (difficulty) ? setTimeout(berzerkGame,3500) : setTimeout(normalGame,3500)
+
+    let i = (difficulty) ? 2 : 1; playTheme(audio[i]);
+
+    opacityToggle(welcomeScreen,overlay,gameboard,credit);
+    displayToggle(gameboard);
+    if (timesplayed > 0) {
+        opacityToggle(gamemode,scoreboard);
+        displayToggle(gamemode,scoreboard);
+    } else startButton.classList.add('hideit');
+
+    let offset = 0;
+    setTimeout(cdText, offset, 'Wait For It!!');
+    setTimeout(cdText, offset += 1000, '3');
+    setTimeout(cdText, offset += 1000, '', '2');
+    setTimeout(cdText, offset += 1000, '', '1');
+    setTimeout(cdText, offset += 500, '', 'SMACK THEM!!');
+    setTimeout(() => {(difficulty) ? berzerkGame() : normalGame()}, offset += 500);
 }
+function gameOver() {
+    timeUp = true;
+    cdText('Game Over!!');    
+    scoreboard.textContent = score;
+    opacityToggle(welcomeScreen,overlay,gameboard,scoreboard,gamemode,credit);
+    displayToggle(gameboard,scoreboard,gamemode);
+}
+
 function normalGame(){
     popOut();
     countingDown();
@@ -83,19 +130,6 @@ function normalGame(){
 function berzerkGame(){
     crazyPopOut();
     countingDown();
-}
-function berzerkToggle(){
-    difficulty = !difficulty;
-    partyButton.classList.toggle('berzerk-deco')
-}
-function pickRandomHole(holes) {
-    const randomHole = Math.floor(Math.random()*holes.length);
-    const hole = holes[randomHole];
-    if (hole === lastHole) {
-        return pickRandomHole(holes);
-    }
-    lastHole = hole;
-    return hole;
 }
 function popOut() {
     const time = Math.random() * 1300 + 400;
@@ -118,53 +152,37 @@ function crazyPopOut() {
         if (!timeUp) crazyPopOut();
     }, time);
 }
+function pickRandomHole(holes) {
+    const randomHole = Math.floor(Math.random()*holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole) {
+        return pickRandomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
 function whack() {
     score++;
     malletAnimation();
-    this.style.backgroundImage = 'url("D:/Work/3.whack-a-mole-gam/images/mole-stunned.png")';
-    this.style.pointerEvents = 'none';
-    setTimeout(() => {
-        this.style.backgroundImage = 'url("D:/Work/3.whack-a-mole-gam/images/mole.png")';
-        this.style.pointerEvents = 'all';
-    }, 1000);
-    scoreboard.textContent = score;
-}
-
-function gameInit(){
-    if (musicon) {bgtheme.play()};
-    opacityToggle(welcomeScreen,overlay,gameboard,credit);
-    displayToggle(gameboard);
-    startButton.classList.add('hideit');
-    if (timesplayed > 0) {
-        opacityToggle(gamemode,scoreboard);
-        displayToggle(gamemode,scoreboard)}
-    scoreboard.textContent = 0;
-    countdownBoard.textContent = 'Wait For It!!';
-    timeUp = false;
-    score = 0;
+    moleAnimation();
+    setTimeout(moleAnimation, Math.random() * 700 + 300);
 }
 
 function countingDown() {
-    let countdown = timeLimit/1000;
+    let countdown = TIMELIMIT/1000;
     let tick = setInterval(() => {
-        countdownBoard.textContent = countdown;
+        cdText(countdown);
         countdown--;
         if (countdown < 0) {
             clearInterval(tick);
             gameOver()};
     }, 1000);
 }
-function gameOver() {
-    timeUp = true;
-    countdownBoard.textContent = 'Game Over!!';
-    opacityToggle(welcomeScreen,overlay,gameboard,scoreboard,gamemode,credit);
-    displayToggle(gameboard,scoreboard,gamemode);
-}
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', gameStart);
 musicButton.addEventListener('click', musicToggle);
 soundButton.addEventListener('click', soundToggle);
-replayButton.addEventListener('click', startGame);
+replayButton.addEventListener('click', gameStart);
 partyButton.addEventListener('click', berzerkToggle);
 
 window.addEventListener('mousemove', cursor);
